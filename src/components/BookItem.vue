4cd18eebd41406c360a2edbebed68049fa50b720
Bonus TM <bonus.tm@gmail.com>
2020-12-03 19:51:09 +0300

    Update book page design, add link to open book on standardebooks.org

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,30 +1,66 @@
 <template>
   <div class="flex">
+    <!-- Mark and Read -->
+    <div class="px-4">
+      <div
+        class="mt-1 mb-10 text-center w-24 p-2 cursor-pointer rounded hover:bg-yellow-50"
+        @click="$emit('toggle', 'read')"
+      >
+        <a :href="book.url" target="_blank" class="text-black no-underline">
+          <img alt="" class="w-14 mx-auto" src="/sebooks-logo-black-full.svg">
+          <div class="text-sm">
+            Get book
+          </div>
+        </a>
+      </div>
+
+      <div
+        class="text-center w-24 mb-2 p-2 cursor-pointer rounded hover:bg-yellow-50"
+        @click="$emit('toggle', 'marked')"
+      >
+        <icon-star :filled="userData.marked" class="mx-auto w-8 h-8" />
+        <div class="text-sm">
+          {{ userData.marked ? 'Marked' : 'Unmarked' }}
+        </div>
+      </div>
+      <div
+        class="text-center w-24 p-2 cursor-pointer rounded hover:bg-yellow-50"
+        @click="$emit('toggle', 'read')"
+      >
+        <icon-read :filled="userData.read" class="mx-auto w-8 h-8" />
+        <div class="text-sm">
+          {{ userData.read ? 'Read' : 'Unread' }}
+        </div>
+      </div>
+    </div>
+
+    <!-- Book Info -->
     <div class="px-2 flex-grow">
-      <h1 class="font-bold">
-        <a :href="book.url">{{ book.title }}</a>
-      </h1>
-      <h3 class="mb-2">
+      <h3 class="text-xl text-yellow-800">
         <span :title="book.authorFullName">{{ book.creator }}</span>
       </h3>
-      <div class="flex justify-start space-x-1 text-xs mb-2">
+      <h1 class="mb-4 font-bold text-5xl text-yellow-800 opacity-80">
+        {{ book.title }}
+      </h1>
+
+      <div class="flex justify-start space-x-1 text-sm mb-2">
         <div
           v-for="(tag, i) in book.tags"
           :key="`tag-${i}`"
-          class="rounded-sm px-1 bg-yellow-600 text-white font-bold"
+          class="rounded px-1 bg-yellow-600 text-white font-bold"
         >
           {{ tag }}
         </div>
       </div>
-      <div class="text-sm">
-        {{ book.wordsCount }}
+      <div class="">
+        {{ Number(book.wordsCount).toLocaleString() }}
         words
       </div>
-      <div class="text-sm mb-4">
-        {{ book.readingEase }}
+      <div class="mb-4">
+        {{ Math.round(book.readingEase) }}
         {{ readingEaseDescription }}
       </div>
-      <div class="text-sm mb-4">
+      <div class="text-purple-800">
         <div
           v-for="(subject, i) in book.subject"
           :key="`subject-${i}`"
@@ -33,34 +69,14 @@
         </div>
       </div>
 
-      <div class="flex">
-        <div
-          class="text-center w-20 mr-2 p-2 cursor-pointer rounded hover:bg-yellow-50"
-          @click="$emit('toggle', 'marked')"
-        >
-          <icon-star :filled="userData.marked" class="mx-auto w-8 h-8" />
-          <div class="text-sm">
-            {{ userData.marked ? 'Marked' : 'Unmarked' }}
-          </div>
-        </div>
-        <div
-          class="text-center w-20 p-2 cursor-pointer rounded hover:bg-yellow-50"
-          @click="$emit('toggle', 'read')"
-        >
-          <icon-read :filled="userData.read" class="mx-auto w-8 h-8" />
-          <div class="text-sm">
-            {{ userData.read ? 'Read' : 'Unread' }}
-          </div>
-        </div>
+      <div class="mt-16 max-w-2xl description" v-html="book.description" />
+    </div>
+
+    <!-- Book cover -->
+    <div class="w-1/3 flex-shrink-0 pl-2">
+      <div class="shadow-lg">
+        <img :src="`/covers/${book.id}.jpg`" alt="" class="object-contain object-left-top">
       </div>
-    </div>
-
-    <div class="px-2 w-1/3 flex-shrink-0">
-      <div class="text-sm description" v-html="book.description" />
-    </div>
-
-    <div class="max-h-96 w-64 flex-shrink-0 pl-2">
-      <img :src="`/covers/${book.id}.jpg`" alt="" class="h-96 object-contain object-left-top">
     </div>
   </div>
 </template>
